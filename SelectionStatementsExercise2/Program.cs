0144365d7db6c3129794a4fb2c5315ca2737ece8
Daniel <dan.r.french@sbcglobal.net>
2023-05-15 13:44:46 -0500

    completed exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("What is your favorite school subject?(please capitalize first letter)");
+            string mySubject = Console.ReadLine();
+
+
+            switch (mySubject)
+            {
+                case "Geometry":
+                    Console.WriteLine($"{mySubject} is my favorite subject too!");
+                    break;
+                case "Calculus":
+                    Console.WriteLine($"{mySubject} is my favorite subject too!");
+                    break;
+                case "Statistics":
+                    Console.WriteLine($"{mySubject} is my favorite subject too!");
+                    break;
+                case "History":
+                    Console.WriteLine($"{mySubject} is my favorite subject too!");
+                    break;
+                case "Biology":
+                    Console.WriteLine($"{mySubject} is my favorite subject too!");
+                    break;
+                default:
+                    Console.WriteLine("We must have missed that one.");
+                    break;
+            }
+            }
     }
 }
